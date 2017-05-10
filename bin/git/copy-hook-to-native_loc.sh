29f2a4bbc8c3b8640833d78298ce6275795e0329
AndrewsOwusu <nanarocky47@gmail.com>
2017-05-10 01:31:48 -0700

    Update copy-hook-to-native_loc.sh

--- bin/git/copy-hook-to-native_loc.sh
+++ bin/git/copy-hook-to-native_loc.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-
+#project custom  hooks location
 custom_home="bin/git/hooks'
+#native hooks location in a given repo
 native_home=".git/hooks"
-
+#change wdir to custom hooks location
 cd $custom_home & ll 
+#copy all hooks in custom hooks location to their native git location and make them executable
 cp .* "$native_home" && chmod +x $i
